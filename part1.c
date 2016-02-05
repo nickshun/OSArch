eafce6185e298c7b501c514129ec43eda02dc44b
Eric Nicholson <eric.nicholson@mymail.champlain.edu>
2016-02-05 12:47:58 -0500

    Added part1

---
+++ part1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int gCounter = 0;
+
+int main ()
+{
+   for (int i = 0; i < 5; ++i)
+   {
+      pid_t pid = fork();
+      
+      if (pid == 0) //if child
+      {
+         while (gCounter != 100)
+         {
+            gCounter++;
+            printf("new counter: %d PID: %d\n", gCounter, getpid());
+         }
+         
+         break;
+      }
+      else if (pid > 0) //if parent
+      {
+      }
+   }
+   
+   wait(NULL);
+   
+   printf("--END PROCESS--\n");
+   
+	return 0;
+}
