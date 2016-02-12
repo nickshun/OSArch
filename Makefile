c7d9f2fca31ccefd2c38130d4697ef48dc06976b
Eric Nicholson <eric.nicholson@mymail.champlain.edu>
2016-02-12 18:04:42 -0500

    yaaay

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: part1.o part2.o
+all: part1.o part2.o part3.o
 
 part1.o:
 	gcc pa1_part1.c -o pa1_part1
@@ -6,12 +6,19 @@ part1.o:
 part2.o:
 	gcc pa1_part2.c -o pa1_part2
 
+part3.o:
+	gcc pa1_part3.c -o pa1_part3
+
 part1: all
 	./pa1_part1
 
 part2: all
 	./pa1_part2
 
+part3: all
+	./pa1_part3
+
 clean:
 	rm pa1_part1
 	rm pa1_part2
+	rm pa1_part3
--- pa1_part3.c
+++ pa1_part3.c
@@ -50,36 +50,21 @@ void *parent(void* pid)
     key_t key;
     int *shm, *s;
     
-    /*
-     * We'll name our shared memory segment
-     * "5678".
-     */
+   
     key = 5678;
     
-    /*
-     * Create the segment.
-     */
+   
     if ((shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666)) < 0) {
         perror("shmget");
         exit(1);
     }
-    printf("created segment in memory\n");
-    /*
-     * Now we attach the segment to our data space.
-     */
-    if ((shm = shmat(shmid, NULL, 0)) == -1) {
+   
+   shm = shmat(shmid, NULL, 0);
+    if (*shm == -1) {
         perror("shmat");
         exit(1);
     }
-    printf("Attached segment to data space\n");
-    /*
-     * Now put some things into the memory for the
-     * other process to read.
-     */
-    //s = shm;
-    
-    //for (c = 0; c <= 100; c++)
-        //s = c;
+   
     s = shm;
     *s = 0;
     printf("set s to 0\n");
@@ -93,16 +78,10 @@ void *parent(void* pid)
             exit(-1);
         }
     }
-    printf("init all children\n");
-    /*
-     * Finally, we wait until the other process
-     * changes the first character of our memory
-     * to '*', indicating that it has read what
-     * we put there.
-     */
-    while (*shm != 100)
-        sleep(1);
-    printf("s is 100\n");
+   
+   for (int i = 0; i < NUM_CHILDREN; i++)
+      pthread_join(children[i], NULL);
+   
     pthread_exit(NULL);
 }
 
@@ -115,57 +94,25 @@ void *child(void* pid)
     int shmid;
     key_t key;
     int *shm, *s;
-    
-    /*
-     * We need to get the segment named
-     * "5678", created by the server.
-     */
+   
     key = 5678;
-    
-    /*
-     * Locate the segment.
-     */
+   
     if ((shmid = shmget(key, SHM_SIZE, 0666)) < 0) {
         perror("shmget");
         exit(1);
     }
     
-    /*
-     * Now we attach the segment to our data space.
-     */
-    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
+    shm = shmat(shmid, NULL, 0);
+    if (*shm == -1) {
         perror("shmat");
         exit(1);
     }
-    
-    /*
-     * Now read what the server put in the memory.
-     */
-     printf("attached memory for sharing\n");
-    for (s = shm; *s != 100; *s++)
+    for (s = shm; *s <= 100; (*s) += 1)
     {
-        printf("s: \n", *s);
         sem_wait(&counter);
         printf("Counter: %d, PID: %d\n", *s,PID);
         sem_post(&counter);
     }
-    //putchar('\n');
-    printf("children got to 100\n");
-    /*
-     * Finally, change the first character of the
-     * segment to '*', indicating we have read
-     * the segment.
-     */
-    // *shm = '*';
-    
-    /*
-    while (s != 100)
-    {
-        sem_wait(&counter);
-        s++;
-        printf("Counter: %d, PID: %d\n", s,PID);
-        sem_post(&counter);
-    }*/
     
     pthread_exit(NULL);
 }
